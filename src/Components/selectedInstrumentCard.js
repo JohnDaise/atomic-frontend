6d1e8474598891ecf647eb281e8b608d056d1d89
JohnDaise <jdaise@gmail.com>
2018-09-24 19:40:44 -0400

    updated modal features

--- src/Components/selectedInstrumentCard.js
+++ src/Components/selectedInstrumentCard.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Grid, Image, Card, Header, Modal, Button } from 'semantic-ui-react'
+import { Grid, Image, Card, Header, Modal, Button, Icon } from 'semantic-ui-react'
 import InstrumentCard from './InstrumentCard'
 
 class SelectedInstrumentCard extends React.Component {
 
+  state = {}
 
+  handleClick = () => this.setState({ active: !this.state.active })
 
 render(){
+   const { active } = this.state
       return(
-        <Modal open={this.props.isModalOpen} onClick={()=> this.props.closeModal()} closeIcon image={"true"} children>
+        <Modal open={this.props.isModalOpen} onClose={()=> this.props.closeModal()} closeIcon={true} image={"true"} children>
          <Modal.Content image centered={"true"} >
             <Grid>
-              <Grid.Column width={8}>
+              <Grid.Column width={5}>
                 <Card>
                 <Card.Header as='h3'>{this.props.instrument.brand} {this.props.instrument.name}</Card.Header>
                 {this.props.instrument.category_id === 2 ?
@@ -19,12 +22,16 @@ render(){
                   :<Image src={this.props.instrument.pic_url} size='small' rounded={true} centered verticalAlign='middle' />}
                 </Card>
                 </Grid.Column>
-                <Grid.Column width={8}>
+                <Grid.Column width={6}>
                 <Card>
                   <Card.Content>
                   <Header as='h5' >
                   Color: {this.props.instrument.color} <br/>
                   Condition: {this.props.instrument.condition}<br/>
+                  Description:
+                    <p>
+                      <em>{this.props.instrument.description}<br/></em>
+                    </p>
                   </Header>
                   <Header as='h4' color='red' >
                   Price: ${this.props.instrument.price}<br/>
@@ -32,7 +39,17 @@ render(){
                   </Card.Content>
                 </Card>
                 </Grid.Column>
-
+                <Grid.Column width={4}>
+                  <div>
+                    <Button.Group >
+                      <Button primary onClick={()=> this.props.closeModal()}>BUY NOW</Button>
+                      <Button secondary toggle active={active} onClick={this.handleClick}>
+                         <Icon name='heart' />
+                        Favorite
+                      </Button>
+                    </Button.Group>
+                  </div>
+                </Grid.Column>
               </Grid>
 
 
